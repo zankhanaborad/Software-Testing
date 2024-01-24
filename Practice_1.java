e0809886b0b82b045e90c8bb0b976faedf20f616
zankhanaborad <110023691+zankhanaborad@users.noreply.github.com>
2024-01-24 12:22:26 +0530

    Add files via upload

---
+++ Practice_1.java
@@ -0,0 +1,56 @@
+package Basic;
+
+public class Practice_1 {
+	public static void main(String[] args) {
+		
+	
+		System.out.println("Zankhana Borad");
+		System.out.println("5 january 1995");
+		System.out.println("29");
+		System.out.println("satyam bunglows,nikol,Ahmedabad");
+		
+		
+		
+		int a =10;
+		int b =20;
+		int c =a+b;
+		System.out.println("addition : "+ c);
+		int d =a-b;
+		System.out.println("subtraction : "+ d);
+		int e = a*b;
+		System.out.println("multiplication : "+ e);
+		int f = a%b;
+		System.out.println("modulo : " + f);
+		int g = a/b;
+		System.out.println("division  : " + g);
+		
+		
+		
+		int r = 5;
+		System.out.println("Area of circle :" +(Math.PI*r*r));
+		int h = a*a;
+		
+		
+		System.out.println("Square formula :" + h);
+		System.out.println("Cube formula :" + (6*h) );
+		System.out.println("Triangle Formula :" + (e/2));
+		int w =12;
+		int l =15;
+		int j =16;
+		
+		int z = w*l;
+		int y = w*j;
+		int v = l*j;
+		int p = z+y+v;
+		
+		System.out.println(" Rectangle Formula :" + z);
+		System.out.println("circumference of Rectangle formula :" + (4*a));
+		int t = a+b+c;
+		
+		System.out.println(" circumference of Triangle formula :" + t);
+		
+		System.out.println(" area of a rectangular prism formula : " +2*p);
+		System.out.println(" circumference of square formula :" + 4*a);
+	}
+
+}
