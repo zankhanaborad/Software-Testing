f3b877e69ec50d794dd62422a2e4b62d675c8e2e
zankhanaborad <110023691+zankhanaborad@users.noreply.github.com>
2024-03-06 11:35:40 +0530

    Add files via upload

--- openbrowser.java
+++ openbrowser.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class openbrowser {
 	public static void main(String[] args) throws InterruptedException {
@@ -22,13 +23,38 @@ public class openbrowser {
 		driver.findElement(By.id("userEmail")).sendKeys("zankhanaborad5195@gmail.com");
 
 		
-		List<WebElement> gender = driver.findElements(By.cssSelector("#genterWrapper > div.col-md-9.col-sm-12 > div:nth-child(2) > label"));
-		gender.get(1).click();
+	driver.findElement(By.xpath("//div[@id='genterWrapper']/div[2]/div[2]")).click();	
 		driver.findElement(By.id("userNumber")).sendKeys("9904366888");
-		driver.findElement(By.cssSelector("#subjectsContainer > div > div.subjects-auto-complete__value-container.subjects-auto-complete__value-container--is-multi.css-1hwfws3")).sendKeys("hello tops Technology");
-		driver.findElement(By.id("currentAddress")).sendKeys("nikol,ahmedabad");
+//
+//		WebElement e= driver.findElement(By.id("subjectsContainer]"));
+//		e.sendKeys("hello");
+		//driver.findElement(By.id("subjectsInput")).click();
+		
+		driver.findElement(By.xpath("//label[text()='Sports']")).click();
+		  WebElement uploadpic = driver.findElement(By.id("uploadPicture"));
+	
+		   uploadpic.sendKeys("C:/Users/STAR/Pictures/pic gps.png"); 
+		   System.out.println("File is Uploaded Successfully");
+		   
+			driver.findElement(By.id("currentAddress")).sendKeys("nikol,ahmedabad");
+			
+		   WebElement state = driver.findElement(By.className("css-1uccc91-singleValue"));
+			Select states = new Select(state);
+			states.selectByIndex(1);
+		
+			
+			  WebElement city = driver.findElement(By.className("css-1uccc91-singleValue"));
+				Select citys = new Select(city);
+				citys.selectByIndex(2);
+
+			
+		driver.findElement(By.id("submit")).click();
+		
 		driver.close();
 
 	}
+	
+	
+
 
 }
