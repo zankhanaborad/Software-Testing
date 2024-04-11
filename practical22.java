14ba189370aca3e0322eeb76d3b9bce81c88ad41
zankhanaborad <110023691+zankhanaborad@users.noreply.github.com>
2024-04-11 12:34:23 +0530

    Add files via upload

---
+++ practical22.java
@@ -0,0 +1,23 @@
+package Basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class practical22 {
+	public static void main(String[] args) {
+		
+		 List<String> list_Strings = new ArrayList<String>();
+		  list_Strings.add("Red");
+		  list_Strings.add("Green");
+		  list_Strings.add("Orange");
+		  list_Strings.add("White");
+		  list_Strings.add("Black");
+
+		  System.out.println(list_Strings);
+	 
+		  list_Strings.remove(2);
+	
+		  System.out.println("After removing third element from the list:\n"+list_Strings);
+	}
+
+}
