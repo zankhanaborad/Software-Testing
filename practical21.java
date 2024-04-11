22dd2946fa795f2172119a0499dc513a069ed987
zankhanaborad <110023691+zankhanaborad@users.noreply.github.com>
2024-04-11 12:31:57 +0530

    Add files via upload

---
+++ practical21.java
@@ -0,0 +1,22 @@
+package Basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class practical21 {
+	public static void main(String[] args) {
+		
+		 List<String> list_Strings = new ArrayList<String>();
+		  list_Strings.add("Red");
+		  list_Strings.add("Green");
+		  list_Strings.add("Orange");
+		  list_Strings.add("White");
+		  list_Strings.add("Black");
+		 
+		  System.out.println(list_Strings);
+		  list_Strings.set(2, "Yellow");
+	
+		  System.out.println(list_Strings);
+	}
+
+}
