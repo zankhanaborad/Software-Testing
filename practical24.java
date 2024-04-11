40a2a4b1a25d87def7cdddfcce99da1ee0e89d58
zankhanaborad <110023691+zankhanaborad@users.noreply.github.com>
2024-04-11 12:40:38 +0530

    Add files via upload

---
+++ practical24.java
@@ -0,0 +1,14 @@
+package Basic;
+
+public class practical24 {
+	public static void main(String[] args) {
+		
+
+	     int arr[] = {10, 20, 30, 40, 50};
+
+	     int n=arr.length;
+	     for(int i=n-1; i>=0; i--)
+	       System.out.print(arr[i]+" ");
+	}
+
+}
