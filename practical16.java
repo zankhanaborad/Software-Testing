e64e08cd0dd5d16957f56a929488bd476dbe2225
zankhanaborad <110023691+zankhanaborad@users.noreply.github.com>
2024-04-11 12:13:45 +0530

    Add files via upload

---
+++ practical16.java
@@ -0,0 +1,16 @@
+package Basic;
+
+import java.util.Arrays;
+
+public class practical16 {
+	public static void main(String[] args) {
+		
+		        int a[]={1,423,6,46,34,23,13,53,4}; 
+		          
+		        Arrays.sort(a);
+		      
+		       
+		        System.out.println("min-"+a[0]+" max-"+a[a.length-1]);
+	}
+
+}
