b279fdc7eeeac1d379da30aa2e20b6df8a9c4ec2
zankhanaborad <110023691+zankhanaborad@users.noreply.github.com>
2024-05-17 09:50:07 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,22 @@
+package Basic;
+
+public class Typecasting {
+public static void main(String[] args) {
+	int i=1;
+	double d=1;
+	
+	System.out.println("i : "+i);
+	System.out.println("d : "+d);
+	
+	int j = (int)3.14;
+	System.out.println(j);
+	
+	
+	int a=10;
+	int b=20;
+	double c = (double)a / (double)b;
+	System.out.println("c:"+c);
+	System.out.println("a :"+a);
+			
+}
+}
